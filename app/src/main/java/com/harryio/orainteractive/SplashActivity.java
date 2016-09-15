91c401cab3489c85d7ac3e3b3790384d2e4d47f3
Harry Singh <me@harryio.com>
2016-09-15 15:28:58 +0530

    Create Splash screen

---
+++ app/src/main/java/com/harryio/orainteractive/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.harryio.orainteractive;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_DISPLAY_DURATION = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_DURATION);
+    }
+}
