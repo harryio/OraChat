e6108473b33de5069d15345410d4bc90a96e2a18
Harry Singh <me@harryio.com>
2016-09-15 16:17:55 +0530

    Create UI for login

---
+++ app/src/main/java/com/harryio/orainteractive/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.harryio.orainteractive;
+
+import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.email)
+    TextInputEditText emailEditText;
+    @BindView(R.id.password)
+    TextInputEditText passwordEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.login, R.id.register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login:
+                break;
+            case R.id.register:
+                break;
+        }
+    }
+}
